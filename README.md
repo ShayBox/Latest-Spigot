420c724d90291e1a74f9755d32b9b5aca5a74d25
Shayne Hartford (ShayBox) <shaybox@shaybox.com>
2019-03-23 22:25:45 -0400

    Added project

---
+++ README.md
@@ -0,0 +1,5 @@
+# LatestSpigot
+A simple Go program that downloads and builds the latest spigot
+
+> This requires the Java Runtime (Oracle, OpenJDK, or compatible)  
+> To check if you have java, Open a powershell/terminal window and type `java -version`
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	println("Deleting Build directory (if it exists)")
+	os.RemoveAll("Build")
+
+	println("Creating Build directory")
+	os.Mkdir("Build", os.ModePerm)
+
+	println("Download BuildTools.jar to Build directory")
+	DownloadFile("Build/BuildTools.jar", "https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar")
+
+	println("Building Spigot/Craftbukkit")
+	cmd := exec.Command("java", "-jar", "BuildTools.jar")
+	cmd.Dir = "./Build"
+	cmd.Start()
+}
+
+// DownloadFile will download a url to a local file. It's efficient because it will
+// write as it downloads and not load the whole file into memory.
+func DownloadFile(filepath string, url string) error {
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	return err
+}
